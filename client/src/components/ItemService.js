cff9e5b341f2303ef8d1833343d637eddc1e42cd
Kevininsky27 <keare27@gmail.com>
2024-06-23 21:17:09 -0600

    arreglos a la app para la solucion de cors

--- client/src/components/ItemService.js
+++ client/src/components/ItemService.js
@@ -3,39 +3,43 @@ import axios from 'axios';
 class ItemService {
     sendData(data) {
         const baseURL = process.env.REACT_APP_PUBLIC_URL;
-        axios.post(`${baseURL}/items/add`, {  // Corrige la ruta aquí
+        axios.post(`${baseURL}/items/add/post`, {
             item: data
         })
         .then((response) => {
-            console.log(response.data);
+            this.setState({
+                item: response.data
+            })
         })
         .catch((error) => {
             console.log(error);
         });
     }
 
-    updateData(data, id) {
+    updateDate(data, id) {
         const baseURL = process.env.REACT_APP_PUBLIC_URL;
-        axios.post(`${baseURL}/items/update/${id}`, {  // Corrige la ruta aquí
+        axios.post(`${baseURL}/items/update/`+id, {
             item: data
         })
         .then((response) => {
-            console.log(response.data);
+            this.asetState({
+                items: response.data
+            })
         })
         .catch((error) => {
-            console.log(error);
-        });
+            console.log(error)
+        })
     }
 
     deleteData(id) {
         const baseURL = process.env.REACT_APP_PUBLIC_URL;
-        axios.delete(`${baseURL}/items/delete/${id}`)
+        axios.delete(`${baseURL}/items/delete/`+id)
         .then(() => {
-            console.log('Deleted');
+            console.log('Deleted')
         })
         .catch((error) => {
-            console.log(error);
-        });
+            console.log(error)
+        })
     }
 }
 
--- server/app.js
+++ server/app.js
@@ -1,50 +1,26 @@
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var port = 4000;
-var cors = require('cors');
-const connectDB = require('./config/db');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const port = 4000;
+const cors = require('cors');
+const connectDB = require ('./config/db');
+
 
 // Conectar a MongoDB
 connectDB();
 
-// Configuración del middleware cors
-var corsOptions = {
-    origin: 'http://shoppr-web-alb-1993009619.us-east-1.elb.amazonaws.com',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    optionsSuccessStatus: 200 // Algunas versiones antiguas de navegadores (IE11, varios SmartTVs) podrían tener problemas con status 204
-};
-
-app.use(cors(corsOptions));
-
-// Middleware para agregar manualmente las cabeceras CORS a todas las respuestas
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://shoppr-web-alb-1993009619.us-east-1.elb.amazonaws.com');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
-    next();
-});
-
-// Middleware para registrar todas las solicitudes (opcional, para depuración)
-app.use((req, res, next) => {
-    console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
-    next();
-});
+// Required aplication specific custom router module
+var itemRouter = require('./src/routes/itemRouter');
 
 // Use middlewares to set view engine and post json data to the server
 app.use(express.static('public'));
+app.use(cors());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
-// Required application-specific custom router module
-var itemRouter = require('./src/routes/itemRouter');
 app.use('/items', itemRouter);
 
 // Start the server
